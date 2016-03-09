7012e325162762b49465735a4a8df37f12171923
wukai <1183323265@qq.com>
2016-03-09 17:23:55 +0800

    first commit

---
+++ content_script.js
@@ -0,0 +1,69 @@
+var keyWords = ["疯狂动物城", "攻壳机动队", "AlphaGo", "papi酱", "Negar"];
+
+var $div = $('<div class="block-info"><p>这里有一个被屏蔽的答案</p><button class="block-btn">手贱一下</button></div>');
+$div.css({
+    "backgroundColor" : "#EFF6FA", 
+    "height":"64px"
+});
+
+var allContents = $(".feed-main");
+for (var i = 0; i < allContents.length; i++) {
+    // debugger;
+    for (var j = 0; j < keyWords.length; j++) {
+        var keyWord = keyWords[j];
+        // if (allContents[i].outerHTML.indexOf(keyWord) >= 0) {
+        //     $(allContents[i]).addClass('hidden');
+        //     $div.clone().insertAfter($(allContents[i]).filter(function() {
+        //         return !$(this).siblings('.block-info').length;
+        //     }));
+        //     continue;
+        // }
+        if (allContents[i].outerHTML.indexOf(keyWord) >= 0 &&
+                $(allContents[i]).siblings('.block-info').length === 0) {
+            $(allContents[i]).addClass('hidden');
+            $div.clone().insertAfter(allContents[i]);
+
+            // 给当前加入的div中的按钮上加入点击事件
+            addBtnEvent($(allContents[i]).siblings('.block-info').children('.block-btn'));
+            break;
+        }       
+    }
+}
+setStyle();
+
+function setStyle() {
+    $('.block-btn').css({
+        "position" : "absolute",
+        "left" : "50%", 
+        "top" : "auto",
+        "bottom" : "12px",
+        "color" : "#fff",
+        "opacity" : ".9",
+        "backgroundColor" : "#81baeb"
+    });
+    $('.block-info>p').css({
+        "font-size" : "0.9em",
+        "padding" : "10px 0 10px 0",
+        "text-align" : "center"
+    });
+}
+
+function addBtnEvent(btn) {
+    btn.on('click', function() {
+    $(this).parent().siblings('.feed-main').toggleClass('hidden');
+    $(this).parent().find('p').toggleClass('hidden');
+
+    // if ($(this).text().indexOf("手贱") >= 0) {
+    //     console.log($(this).text());
+    //     $(this).text("啊，没防备啊！");
+    //     $(this).parent().siblings('.feed-main').removeClass('hidden');
+    //     console.log("remove hidden");
+    //     $(this).parent().find('p').addClass('hidden');
+    // } else {
+    //             console.log($(this).text());
+    //     $(this).text("再手贱一下");
+    //     $(this).parent().siblings('.feed-main').addClass('hidden');
+    //     $(this).parent().find('p').removeClass('hidden');
+    // }
+});
+}
