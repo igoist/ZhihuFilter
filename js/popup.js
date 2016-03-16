b3977b5ec538739520a245f220bfe70024ebde01
wukai <1183323265@qq.com>
2016-03-16 19:00:09 +0800

    修改popup.html的显示函数

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,24 @@
 var keywordsDisplay = localStorage.keywords.split(',');
+displayKeywords();
 
-console.log(keywordsDisplay);
-
-for(var i = 0; i < keywordsDisplay.length; i++) {
-    var wordSpan = document.createElement('span');
-    wordSpan.innerHTML = keywordsDisplay[i];
-    wordSpan.className = "item-word";
-    document.getElementById('display-keywords').appendChild(wordSpan);
+// 将所有想屏蔽的关键词放在单独的span里显示出来
+function displayKeywords() {
+    // 清除已有的 span 元素，防止重复显示。这里不用jQuery怎么写？
+    $('#display-keywords').empty();
+    keywordsDisplay = localStorage.keywords.split(',');
+    for(var i = 0; i < keywordsDisplay.length; i++) {
+        var wordSpan = document.createElement('span');
+        wordSpan.innerHTML = keywordsDisplay[i];
+        wordSpan.className = "item-word";
+        document.getElementById('display-keywords').appendChild(wordSpan);
+    }
 }
+
+// 在输入框内按下回车会触发按钮单击事件
+document.getElementById('new-word').onkeydown = function() {
+    if (event.keyCode == 13)
+    document.getElementById('save').click();
+};
 
 document.getElementById('save').onclick = function(){
     console.log(localStorage.keywords);
@@ -19,7 +30,7 @@ document.getElementById('save').onclick = function(){
         } else {
             localStorage.keywords = newWord;
         }
-        //document.getElementById('display-keywords').innerHTML = '这里是已保存的关键字：<br>' + localStorage.keywords.toString();
+        displayKeywords();
     }
 };
 
